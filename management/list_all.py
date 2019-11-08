a4c7a26d2db0224f071f6349ca131df5ab5437ad
EmersonBraun <emersonfbraun@gmail.com>
2019-11-07 21:24:30 -0300

    add readme

--- management/list_all.py
+++ management/list_all.py
@@ -25,27 +25,31 @@ def count_all():
         return int(row[0])
 
 
+def get_list():
+    sql = """
+        SELECT institution, description, hours_quantity
+        , completion_date FROM certificates
+        ORDER BY completion_date DESC
+        """
+    try:
+        cursor.execute(sql)
+    except ProgrammingError as e:
+        print(f'Error: {e.msg}')
+    else:
+        return cursor.fetchall()
+
 def list_all():
     qt = count_all()
     if qt == 0:
         print('Nothing inserted')
     else:
-        sql = """
-            SELECT institution, description, hours_quantity
-            , completion_date FROM certificates
-            ORDER BY completion_date DESC
-            """
-        try:
-            cursor.execute(sql)
-        except ProgrammingError as e:
-            print(f'Error: {e.msg}')
-        else:
-            columns = ['Name', 'Description', 'Hours', 'Date']
-            print('|{:<20}|{:<60}|{:<5}|{:<10}|'.format(*columns))
-            print('|:{}|:{}|:{}|:{}|'.format(19*'-', 59*'-', 4*'-', 9*'-'))
-            for result in cursor.fetchall():
-                print('|{:<20}|{:<60}|{:<5}|{:<10}|'.format(*result))
-            print('\n\n')
+        result = get_list()
+        columns = ['Name', 'Description', 'Hours', 'Date']
+        print('|{:<20}|{:<60}|{:<5}|{:<10}|'.format(*columns))
+        print('|:{}|:{}|:{}|:{}|'.format(19*'-', 59*'-', 4*'-', 9*'-'))
+        for r in result:
+            print('|{:<20}|{:<60}|{:<5}|{:<10}|'.format(*r))
+        print('\n\n')
 
 
 if __name__ == '__main__':
---
+++ management/update_readme.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+from datetime import date
+from list_all import get_list
+
+print("Create or update README.MD")
+f = open('../README.MD','w', encoding="utf8")
+content = []
+
+print("Header")
+content.append("# List of certificates\n")
+
+print("Date of update")
+content.append(f'## Updated: {date.today()}\n')
+
+print("Get list of certificates")
+result = get_list()
+columns = ['Name', 'Description', 'Hours', 'Date']
+content.append('|{:<20}|{:<60}|{:<5}|{:<10}|\n'.format(*columns))
+content.append('|:{}|:{}|:{}|:{}|\n'.format(19*'-', 59*'-', 4*'-', 9*'-'))
+for r in result:
+    content.append('|{:<20}|{:<60}|{:<5}|{:<10}|\n'.format(*r))
+
+content.append('\n\n')
+f.writelines(content)
+f.close()
